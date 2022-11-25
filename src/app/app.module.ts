432976363168a1b26cc8b4a8710b43b3d53775ae
jobin <jobinjose.in@gmail.com>
2022-11-25 14:44:40 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes = [
+  {
+    path:"",
+    component:ViewProductComponent
+  },
+  {
+    path:"add",
+    component:AddProductComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +28,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
